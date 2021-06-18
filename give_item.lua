1780e62c84689f2b20ca26a81698b9e99788a850
Riceball LEE <snowyu.lee@gmail.com>
2021-06-18 22:20:35 +0800

    fix(giveItem): missing lua extension name

--- give_item.lua
+++ give_item.lua
@@ -1,8 +1,9 @@
 local MOD_PATH = play_challenge.MOD_PATH
 
-local callChatCmd = dofile(MOD_PATH.."call_chat_cmd")
+local callChatCmd = dofile(MOD_PATH.."call_chat_cmd.lua")
 
 local function giveItem(playerName, item, count)
+  if not count then count = 1 end
   return callChatCmd("give", playerName, {item, count})
 end
 
