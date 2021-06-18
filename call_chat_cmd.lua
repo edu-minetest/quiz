f5872d0742d29138182d9f6339ca190e55995654
Riceball LEE <snowyu.lee@gmail.com>
2021-06-18 18:06:37 +0800

    feat: add callChatCmd func

---
+++ call_chat_cmd.lua
@@ -0,0 +1,23 @@
+local chatCommands = minetest.registered_chatcommands
+
+local function callChatCmd(cmdStr, playerName, params)
+  local cmd = chatCommands[cmdStr]
+  if cmd then
+    local paramsType = type(params)
+    if (params == nil or paramsType == "string") then
+      return cmd.func(playerName, params)
+    elseif paramsType ~= "table" then
+      -- if paramsType ~= "table" then
+      --   params = {playerName}
+      -- else
+      --   table.insert(params, 1, playerName)
+      -- end
+
+      return cmd.func(playerName, table.concat(params, " "))
+    end
+  else
+    minetest.log("warning", "callChatCmd: No such chat command:" .. cmdStr)
+  end
+end
+
+return callChatCmd
