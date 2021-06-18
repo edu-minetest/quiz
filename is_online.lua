02b4967b814bf062fb621fe732593ec3d1773600
Riceball LEE <snowyu.lee@gmail.com>
2021-06-18 22:15:55 +0800

    feat" add isOnline func

---
+++ is_online.lua
@@ -0,0 +1,12 @@
+local function isOnline(playerName)
+  local players = minetest.get_connected_players()
+  for _,player in ipairs(players) do
+    local name = player:get_player_name()
+    if (name == playerName) then
+      print(playerName .. " is onlined")
+      return true
+    end
+  end
+end
+
+return isOnline
