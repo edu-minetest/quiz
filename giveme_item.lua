852e8e37cd1543f3bd9a160c67c481af09195963
Riceball LEE <snowyu.lee@gmail.com>
2021-06-20 21:24:43 +0800

    feat(givemeItem): use award item table argument instead

--- giveme_item.lua
+++ giveme_item.lua
@@ -1,10 +1,23 @@
 local MOD_PATH = play_challenge.MOD_PATH
+local S = play_challenge.get_translator
 
 local callChatCmd = dofile(MOD_PATH.."call_chat_cmd.lua")
 
-local function giveItem(playerName, item, count)
-  if not count then count = 1 end
-  return callChatCmd("give", playerName, {item, count})
+local function givemeItem(playerName, item)
+  local name = item.name
+  local modName = item.mod or "default"
+  local count = item.count or 1
+  local title = item.title or name
+  -- local player = minetest.get_player_by_name(playerName)
+  -- player:get_inventory():add_item('main', 'default:mese 1')
+
+  local result = callChatCmd("giveme", playerName, {modName .. ":" .. name, count})
+  if not result then
+    minetest.chat_send_player(playerName, S("Can't get ")..default.get_translator(title))
+  else
+    minetest.chat_send_player(playerName, S("You get the @1, count: @2.", default.get_translator(title), count))
+  end
+  return result
 end
 
-return giveItem
+return givemeItem
