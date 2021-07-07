277fc587f5caa318c206131c24a9fd0b24bb8f0e
Riceball LEE <snowyu.lee@gmail.com>
2021-07-07 18:19:28 +0800

    fix: crashed if award item not used default mod

--- giveme_item.lua
+++ giveme_item.lua
@@ -7,15 +7,18 @@ local function givemeItem(playerName, item)
   local name = item.id
   local modName = item.mod or "default"
   local count = item.count or 1
-  local title = item.title or name
+  local title = item.title or name or ""
+  local T = minetest.get_translator(modName) or S
+
   -- local player = minetest.get_player_by_name(playerName)
   -- player:get_inventory():add_item('main', 'default:mese 1')
 
+  print("giveme Item:", modName, name, dump(item))
   local result = callChatCmd("giveme", playerName, {modName .. ":" .. name, count})
   if not result then
-    minetest.chat_send_player(playerName, S("Can't get '@1'", default.get_translator(title)))
+    minetest.chat_send_player(playerName, S("Can't get '@1'", T(title)))
   else
-    minetest.chat_send_player(playerName, S("You get the @1, count: @2.", default.get_translator(title), count))
+    minetest.chat_send_player(playerName, S("You get the @1, count: @2.", T(title), count))
   end
   return result
 end
