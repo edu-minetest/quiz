0f8dce9dc0c21bc2ef51684fcab24117b13dec2c
Riceball LEE <snowyu.lee@gmail.com>
2021-07-18 22:06:11 +0800

    feat: add mineclone support basically(can extends the quiz_mineclone_items.yml)

--- giveme_item.lua
+++ giveme_item.lua
@@ -1,7 +1,15 @@
+local MOD_NAME = quiz.MOD_NAME
 local MOD_PATH = quiz.MOD_PATH
 local S = quiz.get_translator
-
+--< in Mineclone 2 game
+local isMineClone = minetest.get_modpath("mcl_core") ~= nil
 local callChatCmd = dofile(MOD_PATH.."call_chat_cmd.lua")
+
+local itemMapper
+
+if (isMineClone) then
+  itemMapper = yaml.readConfig(MOD_NAME, "mineclone_items.yml")
+end
 
 local function givemeItem(playerName, item)
   local name = item.id
@@ -9,12 +17,18 @@ local function givemeItem(playerName, item)
   local count = item.count or 1
   local title = item.title or name or ""
   local T = minetest.get_translator(modName) or S
+  local id = modName .. ":" .. name
+  local mcItem = itemMapper and itemMapper[id]
+  if mcItem then
+    id = mcItem.id
+    if mcItem.title then title = mcItem.title end
+  end
 
   -- local player = minetest.get_player_by_name(playerName)
   -- player:get_inventory():add_item('main', 'default:mese 1')
 
   -- print("giveme Item:", modName, name, dump(item))
-  local result = callChatCmd("giveme", playerName, {modName .. ":" .. name, count})
+  local result = callChatCmd("giveme", playerName, {id, count})
   if not result then
     minetest.chat_send_player(playerName, S("Can't get '@1'", T(title)))
   else
