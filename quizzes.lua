4e76222a3df435ce3b044286b703f34250266504
Riceball LEE <snowyu.lee@gmail.com>
2022-07-28 11:38:07 +0800

    refactor: rename to lastCheckedTime and do not persistent the lastCheckedTime too fix(id): hash quiz.title and quiz.answer as id now

--- quizzes.lua
+++ quizzes.lua
@@ -5,15 +5,16 @@ local modstore = quiz.store
 local MOD_PATH = quiz.MOD_PATH
 local S = quiz.get_translator
 local settings = quiz.settings
+local logQuiz = quiz.logQuiz
 
 local toBool = dofile(MOD_PATH .. "to_bool.lua")
-local playerAttrs = dofile(MOD_PATH .. "player_attrs.lua")
+-- local playerAttrs = dofile(MOD_PATH .. "player_attrs.lua")
 local calcType = dofile(MOD_PATH .. "calc_type.lua")
 -- local isArrayEqu = dofile(MOD_PATH .. "array.lua").equal
 
+local getSession = quiz.getSession
+
 local Quizzes = {}
--- record player last answered time
-local lastAnswered = {}
 -- local quizIdPrefix = MOD_NAME .. ":"
 
 --< Current Quiz Index
@@ -22,16 +23,30 @@ local lastAnswered = {}
 -- if currQuiz == nil or currQuiz < 1 then currQuiz = 1 end
 -- local quizzes = settings.quiz
 
-function Quizzes.lastAnswered(playerName)
-  return lastAnswered[playerName]
+local function getLastCheckedTime(playerName)
+  return getSession(playerName).lastChecked or 0
 end
 
-function Quizzes.clearAnswered()
-  lastAnswered = {}
+local function setLastCheckedTime(playerName, value)
+  local session = getSession(playerName)
+  if value == nil then value = os.time() end
+  session.lastChecked = value
+end
+
+function Quizzes.lastChecked(playerName)
+  return getLastCheckedTime(playerName)
 end
 
 local function id(quiz)
-  return quiz.id or minetest.sha1(quiz.answer)
+  local result = quiz.id
+  if result == nil then
+    if quiz.type == "calc" then
+      result = quiz.calc
+    else
+      result = minetest.sha1(quiz.title .. quiz.answer)
+    end
+  end
+  return result
 end
 
 local function setCurrent(playerName, index)
@@ -53,7 +68,7 @@ local function getCurrent(playerName)
     playerName = player:get_player_name()
   end
   if not player then return nil, S("@1 may be offline", playerName) end
-  local attrs = player:get_meta()
+  -- local attrs = player:get_meta()
   local quizzes = settings.quiz
   local currQuiz = modstore:get_int(playerName .. ":currentQuiz")
   if currQuiz == 0 then currQuiz = 1 end
@@ -75,11 +90,12 @@ local function getCurrent(playerName)
   if (not quiz) then return nil, S("No such quiz Id: '@1'", currQuiz) end
   if settings.skipAnswered and settings.skipAnswered > 0 and quiz.type ~= "calc" then
     local index = currQuiz
-    local answered = playerAttrs.getQuiz(attrs, id(quiz)..":answered")
+    local session = getSession(playerName)
+    local answered = session[id(quiz)..":answered"] or 0
     while answered >= settings.skipAnswered and index < #quizzes do
       index = index+1
       quiz = quizzes[currQuiz]
-      answered = playerAttrs.getQuiz(attrs, id(quiz)..":answered")
+      answered = session[id(quiz)..":answered"] or 0
     end
     setCurrent(playerName, index)
     -- print('TCL:: ~ file: quizzes.lua ~ line 81 ~ getCurrent - answered', attrId, answered);
@@ -104,6 +120,11 @@ local function next(playerName)
   setCurrent(playerName, currQuiz)
 end
 
+local function incValue(session, key)
+  local value =  session[key] or 0
+  session[key] = value + 1
+end
+
 local function check(playerName, answer, quiz)
   -- print('TCL:: ~ file: quizzes.lua ~ line 108 ~ check - playerName, answer, quiz', playerName, answer, dump(quiz));
   if not quiz and not answer then
@@ -111,7 +132,7 @@ local function check(playerName, answer, quiz)
     local idletime = settings.idleInterval
     if (idletime) then
       idletime = idletime * 60
-      local lasttime = lastAnswered[playerName] or 0
+      local lasttime = getLastCheckedTime(playerName)
       if lasttime ~= 0 then
         lasttime = os.time() - lasttime
         -- print('TCL:: ~ file: quizzes.lua ~ line 142 ~ check: lasttime', idletime, lasttime);
@@ -131,9 +152,10 @@ local function check(playerName, answer, quiz)
   end
   -- print('TCL:: ~ file: quizzes.lua ~ line 132 ~ check quiz', dump(quiz));
   if quiz then
-    local attrs = player:get_meta()
+    -- local attrs = player:get_meta()
     local quizId= id(quiz)
     if answer ~= nil then
+      local session = getSession(playerName)
       local vRealAnswer = quiz.answer
       local vType = quiz["type"] or type(vRealAnswer)
       if vType == "number" then
@@ -168,22 +190,29 @@ local function check(playerName, answer, quiz)
       elseif answer == vRealAnswer then
         ok = true
       end
+      logQuiz(playerName, quiz, answer, ok)
       if ok then
         if vType == "calc" then
           quiz.calc = nil
         end
+
         local answeredName = quizId ..":answered"
-        local answered = playerAttrs.getQuiz(attrs, answeredName)
-        answered = answered + 1
-        playerAttrs.setQuiz(attrs, answeredName, answered)
-        lastAnswered[playerName] = os.time()
+        incValue(session, answeredName)
+
+        -- local answered = playerAttrs.getQuiz(attrs, answeredName)
+        -- answered = answered + 1
+        -- playerAttrs.setQuiz(attrs, answeredName, answered)
+
+        setLastCheckedTime(playerName, os.time())
         next(playerName)
         return true
       else
         local wrongName = quizId .. ":wrong"
-        local wrong = playerAttrs.getQuiz(attrs, wrongName)
-        wrong = wrong + 1
-        playerAttrs.setQuiz(attrs, wrongName, wrong)
+        incValue(session, wrongName)
+        -- local wrong = playerAttrs.getQuiz(attrs, wrongName)
+        -- wrong = wrong + 1
+        -- playerAttrs.setQuiz(attrs, wrongName, wrong)
+
         return false
       end
     end
